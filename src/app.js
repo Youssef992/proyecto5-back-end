b944a4114d1a20fefd036029735ddfd894397eb3
Manuel Youssef Morales Matta <mmorales@dell1425.tbn.com.mx>
2022-08-23 16:10:25 -0500

    Corrección Cors()

--- src/app.js
+++ src/app.js
@@ -4,8 +4,9 @@ const apiRouter=require('./apis');
 const app = express();
 
 app.use(express.json());
-app.use('/api/v1',apiRouter);
 app.use(cors());
+app.use('/api/v1',apiRouter);
+
 
 app.use('/',(req,res)=>{
     res.send({message:'esto funciona'});
